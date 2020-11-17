1669b4a6253c92e74675c3f746e1b04ccf61d5c5
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-17 09:27:17 -0300

    feat: use puppeteer to get info in my website

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const puppeteer = require("puppeteer");
+const fs = require("fs");
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("https://rafaelgoulartb.vercel.app/");
-  await page.screenshot({ path: "results/screenshots/example.png" });
+
+  const resultList = await page.evaluate(() => {
+    const nodeList = document.querySelectorAll("#portfolio div h2");
+
+    const projectsTitleArray = [...nodeList];
+
+    const list = projectsTitleArray.map((item) => {
+      return {
+        title: item.textContent,
+      }
+    });
+
+    return list
+  });
+
+  fs.writeFile('projects.json', JSON.stringify(resultList, null, 2), err => {
+    if(err) {
+      console.error(err)
+      throw new Error('Something went wrong')}
+    else console.log('ok')
+  })
 
   await browser.close();
 })();
